2251215b6c6457ac34d70d7608ff7d7cf4d171db
Yrobot <Yrobot@qq.com>
2021-05-27 20:37:12 +0800

    支持onClick事件

--- README.md
+++ README.md
@@ -9,7 +9,8 @@
 5. 展开列后可以左右滑动 table 查看
 6. 点击展开列可以恢复收缩状态
 
-## 利用codesandbox查看
+## 利用 codesandbox 查看
+
 [view in codesandbox](https://codesandbox.io/s/react-mobile-table-jrled?file=/src/App.js)
 
 ## 查看 demo
@@ -27,33 +28,50 @@ yarn add @yrobot/react-mobile-table
 
 2. 引入组件并使用
 
-```
-import MobileTable from '@yrobot/react-mobile-table'
-import '@yrobot/react-mobile-table/lib/index.css'
+```js
+import MobileTable from '@yrobot/react-mobile-table';
+import '@yrobot/react-mobile-table/lib/index.css';
 
 const data = [
-	['ID', 'Name', 'Age', 'Address'], // title line
-	['1', 'John Brown', 32, 'New York No. 1 Lake Park, New York No. 1 Lake Park'],
-	['2', 'Jim Green', 42, 'London No. 2 Lake Park, London No. 2 Lake Park'],
-	['3', 'Joe Black', 32, 'Sidney No. 1 Lake Park, Sidney No. 1 Lake Park'],
-	['4', 'Joe Green jsdkk sdkksdqww', 36, 'Sidney NOOOOOO'],
+  ['ID', 'Name', 'Age', 'Address'], // title line
+  ['1', 'John Brown', 32, 'New York No. 1 Lake Park, New York No. 1 Lake Park'],
+  ['2', 'Jim Green', 42, 'London No. 2 Lake Park, London No. 2 Lake Park'],
+  ['3', 'Joe Black', 32, 'Sidney No. 1 Lake Park, Sidney No. 1 Lake Park'],
+  ['4', 'Joe Green jsdkk sdkksdqww', 36, 'Sidney NOOOOOO'],
 ];
 
 <MobileTable
-	data={data} // 数据源:[[]]
-	columnMinWidth={60} // 每一列的最小宽度, 假设屏宽为375, 默认值60: Int
-/>
-
+  data={data} // 数据源:[[]]
+  columnMinWidth={60} // 每一列的最小宽度, 假设屏宽为375, 默认值60: Int
+  // table内部点击事件回调
+  onClick={(param) => {
+    const {
+      column, //第几列，从0开始
+      line, // 第几行，从0开始，包含header行
+      collapse, // 当前列是否折叠,boolean
+      event,
+    } = param;
+    console.log(param);
+  }}
+/>;
 ```
 
-## 版本更新log
+## 版本更新 log
+
+#### v1.2.0 [feature]
+
+1. 支持 onClick 事件，获取点击行列和展开状态
+
 #### v1.1.4 [fix bug]
-1. 修复使用CRA安装mobile-table时会安装两个版本react
+
+1. 修复使用 CRA 安装 mobile-table 时会安装两个版本 react
 
 #### v1.1.3 [fix bug]
-1. 适配仓库兼容react版本15（使用memoize-one缓存计算结果）  
-2. 组件库添加terser压缩js代码  
-3. demo添加分页效果
+
+1. 适配仓库兼容 react 版本 15（使用 memoize-one 缓存计算结果）
+2. 组件库添加 terser 压缩 js 代码
+3. demo 添加分页效果
 
 #### v1.1.2 [fix bug]
-1. 添加line-height，修复特殊字体中英文行高不一致的情况
+
+1. 添加 line-height，修复特殊字体中英文行高不一致的情况
--- src/index.js
+++ src/index.js
@@ -12,105 +12,116 @@ const getArray = (len) => (fn) => [...Array(len)].map((v, i) => fn(i));
 
 // 获取string的字节宽度：中文字符=》2字节宽
 String.prototype.getCharSize = function () {
-	return this.replace(/[^\u0000-\u00ff]/g, '  ').length;
+  return this.replace(/[^\u0000-\u00ff]/g, '  ').length;
 };
 
 // 根据传入的宽度百分比，自动优化，防止过小的列
 const autoSize = memoize((arr = [], MIN_SIZE) => {
-	// const MIN_SIZE = (Math.max(...titles.map((v) => (v + '').getCharSize())) / 2) * fontSize + 16 // 根据(最长title字长+padding=计算MIN_SIZE)计算MIN_SIZE
-	const MIN_WIDTH_P = (MIN_SIZE / 375) * 100;
-	const LEN = arr.length;
+  // const MIN_SIZE = (Math.max(...titles.map((v) => (v + '').getCharSize())) / 2) * fontSize + 16 // 根据(最长title字长+padding=计算MIN_SIZE)计算MIN_SIZE
+  const MIN_WIDTH_P = (MIN_SIZE / 375) * 100;
+  const LEN = arr.length;
 
-	// 如果平均宽度都到不了MIN_WIDTH_P，直接返回arr
-	if (MIN_WIDTH_P > 100 / LEN) return arr;
+  // 如果平均宽度都到不了MIN_WIDTH_P，直接返回arr
+  if (MIN_WIDTH_P > 100 / LEN) return arr;
 
-	let subCount = 0; // 记录所有小于MIN_WIDTH_P的缺值和
-	let overCount = 0; // 记录所有大于MIN_WIDTH_P的超值和
-	arr.map((v) => {
-		if (v > MIN_WIDTH_P) {
-			overCount += v - MIN_WIDTH_P;
-		} else {
-			subCount += MIN_WIDTH_P - v;
-		}
-	});
+  let subCount = 0; // 记录所有小于MIN_WIDTH_P的缺值和
+  let overCount = 0; // 记录所有大于MIN_WIDTH_P的超值和
+  arr.map((v) => {
+    if (v > MIN_WIDTH_P) {
+      overCount += v - MIN_WIDTH_P;
+    } else {
+      subCount += MIN_WIDTH_P - v;
+    }
+  });
 
-	if (subCount === 0) return arr; // 所有数据都大于MIN_WIDTH_P，直接返回arr
+  if (subCount === 0) return arr; // 所有数据都大于MIN_WIDTH_P，直接返回arr
 
-	const overRate = (overCount - subCount) / overCount; // 计算超出值的缩放比例
+  const overRate = (overCount - subCount) / overCount; // 计算超出值的缩放比例
 
-	return arr.map((v) => {
-		if (v > MIN_WIDTH_P) {
-			return MIN_WIDTH_P + (v - MIN_WIDTH_P) * overRate;
-		} else {
-			return MIN_WIDTH_P;
-		}
-	});
+  return arr.map((v) => {
+    if (v > MIN_WIDTH_P) {
+      return MIN_WIDTH_P + (v - MIN_WIDTH_P) * overRate;
+    } else {
+      return MIN_WIDTH_P;
+    }
+  });
 });
 
 // 根据传入的data配置每一列的宽度百分比
 const getWidthPercent = memoize((data, titleWeigth = 10, contentWeight = 1) => {
-	const widthLen = [];
-	let count = 0;
-	data.map((arr = [], index) => {
-		const base = (index > 0 ? contentWeight : titleWeigth) / 1;
-		arr.map((item, i) => {
-			widthLen[i] = (widthLen[i] || 0) + (item + '').getCharSize() * base;
-		});
-	});
-	widthLen.map((len) => {
-		count += len;
-	});
-	return widthLen.map((v) => (v * 100) / count);
+  const widthLen = [];
+  let count = 0;
+  data.map((arr = [], index) => {
+    const base = (index > 0 ? contentWeight : titleWeigth) / 1;
+    arr.map((item, i) => {
+      widthLen[i] = (widthLen[i] || 0) + (item + '').getCharSize() * base;
+    });
+  });
+  widthLen.map((len) => {
+    count += len;
+  });
+  return widthLen.map((v) => (v * 100) / count);
 });
 
 class MobileTable extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-	switchFold(i) {
-		const { data = [[]] } = this.props;
-		const { columnFolds = data[0].map(() => true) } = this.state;
-		let newColumnFolds = columnFolds.slice();
-		newColumnFolds[i] = !newColumnFolds[i];
-		this.setState({
-			columnFolds: newColumnFolds,
-		});
-	}
+  switchFold(i) {
+    const { data = [[]] } = this.props;
+    const { columnFolds = data[0].map(() => true) } = this.state;
+    let newColumnFolds = columnFolds.slice();
+    newColumnFolds[i] = !newColumnFolds[i];
+    this.setState({
+      columnFolds: newColumnFolds,
+    });
+  }
 
-	render() {
-		const { data = [[]], columnMinWidth = 69 } = this.props;
-		const widthPercent = autoSize(getWidthPercent(data), columnMinWidth);
-		const { columnFolds = data[0].map(() => true) } = this.state;
-		const lineSize = data.length || 0;
-		const columnSize = data[0].length || 0;
-		return (
-			<div className='react-mobile-table'>
-				<div className='react-mobile-table-column-holder'>
-					{getArray(columnSize)((columnIndex) => (
-						<div
-							key={`cloumn${columnIndex}`}
-							className='react-mobile-table-table-column'
-							style={{
-								minWidth: widthPercent[columnIndex] + '%',
-								width: !!columnFolds[columnIndex] ? widthPercent[columnIndex] + '%' : 'auto',
-							}}
-							onClick={() => {
-								this.switchFold(columnIndex);
-							}}
-						>
-							{getArray(lineSize)((lineIndex) => (
-								<div key={`cloumn${columnIndex}line${lineIndex}`} className='react-mobile-table-item'>
-									{data[lineIndex][columnIndex]}
-								</div>
-							))}
-						</div>
-					))}
-				</div>
-			</div>
-		);
-	}
+  render() {
+    const { data = [[]], columnMinWidth = 69, onClick = () => {} } = this.props;
+    const widthPercent = autoSize(getWidthPercent(data), columnMinWidth);
+    const { columnFolds = data[0].map(() => true) } = this.state;
+    const lineSize = data.length || 0;
+    const columnSize = data[0].length || 0;
+    return (
+      <div className='react-mobile-table'>
+        <div className='react-mobile-table-column-holder'>
+          {getArray(columnSize)((columnIndex) => (
+            <div
+              key={`cloumn${columnIndex}`}
+              className='react-mobile-table-table-column'
+              style={{
+                minWidth: widthPercent[columnIndex] + '%',
+                width: !!columnFolds[columnIndex] ? widthPercent[columnIndex] + '%' : 'auto',
+              }}
+              onClick={() => {
+                this.switchFold(columnIndex);
+              }}
+            >
+              {getArray(lineSize)((lineIndex) => (
+                <div
+                  key={`cloumn${columnIndex}line${lineIndex}`}
+                  className='react-mobile-table-item'
+                  onClick={(event) => {
+                    onClick({
+                      column: columnIndex,
+                      line: lineIndex,
+                      collapse: !columnFolds[columnIndex],
+                      event,
+                    });
+                  }}
+                >
+                  {data[lineIndex][columnIndex]}
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default MobileTable;
